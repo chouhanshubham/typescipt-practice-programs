55d3d670cff2d08f39f11aed925e13f2d5c57a5c
Shubham <shubh@hotmail.co.in>
2026-08-13 00:07:30 +0530

    count the numbers

---
+++ Day 6/count.ts
@@ -0,0 +1,9 @@
+let number = [1,3,5,5,6,7,8,8];
+let count = 0;
+
+for (let i=0 ; i < number.length; i++) {
+    count ++;
+
+}
+console.log(`The total number of elements in the array is: ${count}`);
+
