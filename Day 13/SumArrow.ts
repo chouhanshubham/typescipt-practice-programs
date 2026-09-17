37349dded02d34017bf86b32b476d8c3c98c9439
Shubham <shubh@hotmail.co.in>
2026-09-18 00:50:53 +0530

    Sum of all Arrays

---
+++ Day 13/SumArrow.ts
@@ -0,0 +1,11 @@
+// Sum of array elements
+
+let arr1 : number[] = [1, 2, 5, 7, 5, 6];
+let sum:number = 0;
+
+for (let i=0;i<arr1.length;i++){
+     sum = sum + arr1[i];
+    
+}
+
+console.log(sum);
